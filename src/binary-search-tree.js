2183644f5b2e34377796d7445bd16805db4c4e78
Olga <fedorako1997@mail.ru>
2025-01-03 00:57:41 +0300

    feat: add part of tree task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,31 +1,60 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const createdNode = new Node(data);
+    if (this.rootNode === null) {
+      this.rootNode = createdNode;
+      return;
+    }
+    let currentNode = this.rootNode;
+        while (true) {
+            if (data < currentNode.data) {
+                if (!currentNode.left) {
+                    currentNode.left = createdNode;
+                    return;
+                }
+                currentNode = currentNode.left;
+            } else {
+                if (!currentNode.right) {
+                    currentNode.right = createdNode;
+                    return;
+                }
+                currentNode = currentNode.right;
+            }
+        }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.find(data) !== null;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let currentNode = this.rootNode;
+    while (currentNode) {
+        if (data === currentNode.data) {
+            return currentNode;
+        } else if (data < currentNode.data) {
+            currentNode = currentNode.left;
+        } else {
+            currentNode = currentNode.right;
+        }
+    }
+    return null;
   }
 
   remove(/* data */) {
